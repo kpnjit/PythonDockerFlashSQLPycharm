56cefc373f03228c8b814e2d69c9e8285a9b11a4
Keerthana <kp79@njit.edu>
2021-07-21 20:44:15 +0530

    Task:Updated  file

--- db/init.sql
+++ db/init.sql
@@ -3,8 +3,8 @@ use citiesData;
 
 CREATE TABLE IF NOT EXISTS tblCitiesImport (
     `fldName` VARCHAR(21) CHARACTER SET utf8,
-    `fldLat` NUMERIC(6, 4),
     `fldLong` NUMERIC(7, 4),
+    `fldLat` NUMERIC(6, 4),
     `fldCountry` VARCHAR(19) CHARACTER SET utf8,
     `fldAbreviation` VARCHAR(3) CHARACTER SET utf8,
     `fldCapitalStatus` VARCHAR(7) CHARACTER SET utf8,
