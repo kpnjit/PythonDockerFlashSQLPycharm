b2a6606dedb9f66c2f624afa2215c4dd719f82f5
Keerthana <kp79@njit.edu>
2021-08-02 01:26:34 +0530

    Merge branch 'Projectt3'
    # Conflicts:
    #	README.MD
    #	app/template/edit.html
    #	app/template/index.html
    #	app/template/new.html
    #	app/template/view.html

--- db/init.sql
+++ db/init.sql
@@ -9,3 +9,9 @@ CREATE TABLE IF NOT EXISTS oscarAgeMale (
     `movie` VARCHAR(43) ,
     PRIMARY KEY (`id`)
 );
+INSERT INTO oscarAgeMale VALUES
+(1,2016,41,'Leonardo Dicaprio','The Revanant'),
+(2,2015,33,'Eddie Redmayne','The Theory of Everything'),
+(3,2014,44,'Matthew McConaughey','Dallas Buyers Club'),
+(4,2013,55,'Daniel Day-Lewis','Lincoln'),
+(5,2012,39,'Jean Dujardin','The Artist');
